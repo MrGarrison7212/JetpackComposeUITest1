af89535d770889b4ffefad7a06d201af089da5e8
MrGarrison7212 <bokipfc1992@gmail.com>
2024-04-25 11:20:19 +0200

    Adding placeholder for reviews part.

--- app/src/main/java/com/example/jetpackcomposeuitest1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeuitest1/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -25,7 +28,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposeuitest1.ui.theme.JetpackComposeUITest1Theme
 import androidx.constraintlayout.compose.ConstraintLayout
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,5 +90,18 @@ fun profile() {
             fontSize = 20.sp,
             color = Color(android.graphics.Color.parseColor("#747679"))
         )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 10.dp, bottom = 10.dp)
+                .height(55.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(android.graphics.Color.parseColor("#ffffff"))
+            ),
+            shape = RoundedCornerShape( 15)
+        ) {
+
+        }
     }
 }
